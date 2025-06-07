5a35614f48219ec9eb3809566794388dc8ca37c9
Soverias <soverias@gmail.com>
2025-06-07 10:29:27 +0200

    basic example working

--- src/app/utilities/chat/chat.ts
+++ src/app/utilities/chat/chat.ts
@@ -1,11 +1,99 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr'
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat.html',
   styleUrl: './chat.css'
 })
-export class Chat {
+export class Chat implements OnInit {
+  public userName = '';
+  public groupName = '';
+  public messageToSend = '';
+  public joined = false;
+  public conversation: NewMessage[] = [{
+    message: 'Bienvenido',
+    userName: 'Sistema'
+  }];
 
+  private connection: HubConnection;
+
+  constructor(private cdr: ChangeDetectorRef) {
+    this.connection = new HubConnectionBuilder()
+      .withUrl('http://localhost:5253/chathub')
+      .build();
+
+    this.connection.on("NewUser", message => this.newUser(message));
+    this.connection.on("NewMessage", message => this.newMessage(message));
+    this.connection.on("LeftUser", message => this.leftUser(message));
+  }
+
+  ngOnInit(): void {
+    this.connection.start()
+      .then(_ => {
+        console.log('Connection Started');
+      }).catch(error => {
+        return console.error(error);
+      });
+  }
+
+  public join() {
+    this.connection.invoke('JoinGroup', this.groupName, this.userName)
+      .then(_ => {
+        this.joined = true;
+        this.cdr.detectChanges(); // Notifica a Angular
+      });
+  }
+
+  public sendMessage() {
+    const newMessage: NewMessage = {
+      message: this.messageToSend,
+      userName: this.userName,
+      groupName: this.groupName
+    };
+
+    this.connection.invoke('SendMessage', newMessage)
+      .then(_ => {
+        this.messageToSend = '';
+        this.cdr.detectChanges(); 
+      });
+  }
+
+  public leave() {
+    this.connection.invoke('LeaveGroup', this.groupName, this.userName)
+      .then(_ => {
+        this.joined = false;
+        this.cdr.detectChanges(); 
+      });
+  }
+
+  private newUser(message: string) {
+    console.log(message);
+    this.conversation.push({
+      userName: 'Sistema',
+      message: message
+    });
+    this.cdr.detectChanges(); 
+  }
+
+  private newMessage(message: NewMessage) {
+    this.conversation.push(message);
+    this.cdr.detectChanges();
+  }
+
+  private leftUser(message: string) {
+    this.conversation.push({
+      userName: 'Sistema',
+      message: message
+    });
+    this.cdr.detectChanges(); 
+  }
+
+}
+
+interface NewMessage {
+  userName: string;
+  message: string;
+  groupName?: string;
 }
